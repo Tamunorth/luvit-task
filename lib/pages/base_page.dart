bbdf0375d0c71f45be461485d33b2f4911748876
Tamunorth <daviesmanuelfavour@gmail.com>
2024-05-03 11:02:37 +0100

    clean up

--- lib/pages/base_page.dart
+++ lib/pages/base_page.dart
@@ -5,6 +5,8 @@ import 'package:luvit_task/pages/home_page.dart';
 
 import 'package:svg_flutter/svg.dart';
 
+import '../widgets/gap.dart';
+
 class BasePage extends StatefulWidget {
   const BasePage({super.key});
 
@@ -30,110 +32,112 @@ class _BasePageState extends State<BasePage> {
           HomePage(),
         ],
       ),
-      bottomNavigationBar: Container(
-        height: 90,
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        decoration: BoxDecoration(
-          color: Colors.black,
-          // shape: ConvexNotchedRectangle(),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              CustomNavItem(
-                icon: Assets.assetsNavHome,
-                index: 0,
-                selectedIndex: _selectedIndex,
-                onTap: _switchNavPage,
-                title: '홈',
-              ),
-              CustomNavItem(
-                icon: Assets.assetsNavLocation,
-                index: 1,
-                selectedIndex: _selectedIndex,
-                onTap: _switchNavPage,
-                title: '스팟',
-              ),
-              // CustomNavItem(
-              //   icon: Icons.home_rounded,
-              //   index: 2,
-              //   selectedIndex: _selectedIndex,
-              //   onTap: _switchNavPage,
-              //   title: 'Home',
-              // ),
-              const SizedBox(
-                width: 65,
-                height: 65,
-              ),
-              CustomNavItem(
-                icon: Assets.assetsNavChat,
-                index: 3,
-                selectedIndex: _selectedIndex,
-                onTap: _switchNavPage,
-                title: '채팅',
-              ),
-              CustomNavItem(
-                icon: Assets.assetsNavPerson,
-                index: 4,
-                selectedIndex: _selectedIndex,
-                onTap: _switchNavPage,
-                title: '마이',
-              ),
-            ]),
-            Positioned(
-              top: -10,
-              bottom: 0,
-              right: 0,
-              left: 0,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Container(
-                  height: 62,
-                  decoration: BoxDecoration(
-                    color: const Color(0xff0E0D0D),
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.black),
-                  ),
-                ),
-              ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            decoration: BoxDecoration(
+              color: Colors.black,
+              // shape: ConvexNotchedRectangle(),
+              borderRadius: BorderRadius.circular(12),
             ),
-            Positioned(
-              top: -4,
-              bottom: 0,
-              right: 0,
-              left: 0,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Container(
-                  height: 50,
-                  decoration: const BoxDecoration(
-                    // color: Theme.of(context).colorScheme.outline,
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      stops: [0.3, 0.8],
-                      colors: [
-                        Color(0xff101010),
-                        Color(0xff2F2F2F),
-                      ],
-                    ),
-                    shape: BoxShape.circle,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        Assets.assetsNavStar,
+            child: SafeArea(
+              bottom: true,
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CustomNavItem(
+                          icon: Assets.assetsNavHome,
+                          index: 0,
+                          selectedIndex: _selectedIndex,
+                          onTap: _switchNavPage,
+                          title: '홈',
+                        ),
+                        CustomNavItem(
+                          icon: Assets.assetsNavLocation,
+                          index: 1,
+                          selectedIndex: _selectedIndex,
+                          onTap: _switchNavPage,
+                          title: '스팟',
+                        ),
+                        const SizedBox(
+                          width: 65,
+                          height: 65,
+                        ),
+                        CustomNavItem(
+                          icon: Assets.assetsNavChat,
+                          index: 3,
+                          selectedIndex: _selectedIndex,
+                          onTap: _switchNavPage,
+                          title: '채팅',
+                        ),
+                        CustomNavItem(
+                          icon: Assets.assetsNavPerson,
+                          index: 4,
+                          selectedIndex: _selectedIndex,
+                          onTap: _switchNavPage,
+                          title: '마이',
+                        ),
+                      ]),
+                  Positioned(
+                    top: -10,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        height: 62,
+                        decoration: BoxDecoration(
+                          color: const Color(0xff0E0D0D),
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.black),
+                        ),
                       ),
-                    ],
+                    ),
                   ),
-                ),
+                  Positioned(
+                    top: -4,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          // color: Theme.of(context).colorScheme.outline,
+                          gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            stops: [0.3, 0.8],
+                            colors: [
+                              Color(0xff101010),
+                              Color(0xff2F2F2F),
+                            ],
+                          ),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              Assets.assetsNavStar,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
